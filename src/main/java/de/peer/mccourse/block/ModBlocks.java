6053b6b1352264d543fdb73b915a615cb6d1bdb3
Peer Limburg <peerlimburg@gmail.com>
2022-01-22 17:47:51 +0100

    Added a custom CreativeModeTab

--- src/main/java/de/peer/mccourse/block/ModBlocks.java
+++ src/main/java/de/peer/mccourse/block/ModBlocks.java
@@ -1,6 +1,7 @@
 package de.peer.mccourse.block;
 
 import de.peer.mccourse.MCCourseMod;
+import de.peer.mccourse.item.ModCreativeModeTab;
 import de.peer.mccourse.item.ModItems;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
@@ -23,19 +24,19 @@ public class ModBlocks {
 
     public static final RegistryObject<Block> COBALT_BLOCK = registerBlock("cobalt_block",
             () -> new Block(BlockBehaviour.Properties.of(Material.METAL)
-                    .strength(5f).requiresCorrectToolForDrops()), CreativeModeTab.TAB_MISC);
+                    .strength(5f).requiresCorrectToolForDrops()), ModCreativeModeTab.COURSE_TAB);
 
     public static final RegistryObject<Block> COBALT_ORE = registerBlock("cobalt_ore",
             () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(4f).requiresCorrectToolForDrops()), CreativeModeTab.TAB_MISC);
+                    .strength(4f).requiresCorrectToolForDrops()), ModCreativeModeTab.COURSE_TAB);
 
     public static final RegistryObject<Block> DEEPSLATE_COBALT_ORE = registerBlock("deepslate_cobalt_ore",
             () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(9999f).requiresCorrectToolForDrops()), CreativeModeTab.TAB_MISC);
+                    .strength(9999f).requiresCorrectToolForDrops()), ModCreativeModeTab.COURSE_TAB);
 
     public static final RegistryObject<Block> RAW_COBALT_BLOCK = registerBlock("raw_cobalt_block",
             () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(1f).requiresCorrectToolForDrops()), CreativeModeTab.TAB_MISC);
+                    .strength(1f).requiresCorrectToolForDrops()), ModCreativeModeTab.COURSE_TAB);
 
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
